02890a6ea9a2f22546a7f086f92c29475ff48593
BCKim55 <tempus29@gmail.com>
2026-02-09 13:55:38 +0900

    3D_Test_case_file_Ma=1.22,Re=500_Single_particle

--- run/2D_ibm_cylinder/case.py
+++ run/2D_ibm_cylinder/case.py
@@ -3,105 +3,98 @@ import math
 
 Mu = 1.84e-05
 gam_a = 1.4
-gam_b = 1.1
 
 # Configuring case dictionary
 print(
     json.dumps(
         {
-            # Logistics
-            "run_time_info": "T",
-            # Computational Domain Parameters
-            # axial direction
-            "x_domain%beg": 0.0e00,
-            "x_domain%end": 1.0e-02,
-            # r direction
-            "y_domain%beg": 0.0e00,
-            "y_domain%end": 6.0e-03,
-            "cyl_coord": "F",
-            "m": 600,
-            "n": 360,
-            "p": 0,
-            "dt": 1.0e-10,
-            "t_step_start": 0,
-            "t_step_stop": 7200,  # 3000
-            "t_step_save": 30,  # 10
-            # Simulation Algorithm Parameters
-            # Only one patches are necessary, the air tube
-            "num_patches": 1,
-            # Use the 5 equation model
-            "model_eqns": 2,
-            # 6 equations model does not need the K \div(u) term
-            "alt_soundspeed": "F",
-            # One fluids: air
-            "num_fluids": 2,
-            # time step
-            "mpp_lim": "F",
-            # Correct errors when computing speed of sound
-            "mixture_err": "T",
-            # Use TVD RK3 for time marching
-            "time_stepper": 3,
-            # Reconstruct the primitive variables to minimize spurious
-            # Use WENO5
-            "weno_order": 5,
-            "weno_eps": 1.0e-6,
-            "weno_Re_flux": "F",
-            "weno_avg": "T",
-            "avg_state": 2,
-            # Use the mapped WENO weights to maintain monotinicity
-            "mapped_weno": "T",
-            "null_weights": "F",
-            "mp_weno": "T",
-            # Use the HLLC  Riemann solver
-            "riemann_solver": 2,
-            "wave_speeds": 1,
-            # We use reflective boundary conditions at octant edges and
-            # non-reflective boundary conditions at the domain edges
-            "bc_x%beg": -3,
-            "bc_x%end": -3,
-            "bc_y%beg": -1,
-            "bc_y%end": -1,
-            # Set IB to True and add 1 patch
-            "ib": "T",
-            "num_ibs": 1,
-            # Formatted Database Files Structure Parameters
-            "format": 1,
-            "precision": 2,
-            "prim_vars_wrt": "T",
-            "E_wrt": "T",
-            "parallel_io": "T",
-            # Patch: Constant Tube filled with air
-            # Specify the cylindrical air tube grid geometry
-            "patch_icpp(1)%geometry": 3,
-            "patch_icpp(1)%x_centroid": 5.0e-03,
-            # Uniform medium density, centroid is at the center of the domain
-            "patch_icpp(1)%y_centroid": 3.0e-03,
-            "patch_icpp(1)%length_x": 1.0e-02,
-            "patch_icpp(1)%length_y": 6.0e-03,
-            # Specify the patch primitive variables
-            "patch_icpp(1)%vel(1)": 0.1e00,
-            "patch_icpp(1)%vel(2)": 0.0e00,
-            "patch_icpp(1)%pres": 1.0e00,
-            "patch_icpp(1)%alpha_rho(1)": 1.0e00,
-            "patch_icpp(1)%alpha(1)": 1.0e00,
-            "patch_icpp(1)%alpha_rho(2)": 0.0e00,
-            "patch_icpp(1)%alpha(2)": 0.0e00,
-            # Patch: cylinder Immersed Boundary
-            "patch_ib(1)%geometry": 2,
-            "patch_ib(1)%x_centroid": 1.05e-03,
-            "patch_ib(1)%y_centroid": 3.0e-03,
-            "patch_ib(1)%radius" : 2.5e-4,
-            "patch_ib(1)%p": 0.4,
-            "patch_ib(1)%m": 0.02,
-            # Fluids Physical Parameters
-            # Use the same stiffness as the air bubble
-            "viscous": "F",
-            "fluid_pp(1)%gamma": 1.0e00 / (gam_a - 1.0e00),  # 2.50 (Not 1.40)
-            "fluid_pp(1)%pi_inf": 0,
-            "fluid_pp(2)%gamma": 1.0e00 / (gam_b - 1.0e00),  # 2.50 (Not 1.40)
-            "fluid_pp(2)%pi_inf": 0,
-            #"fluid_pp(1)%Re(1)" : 200.0,
-            #"fluid_pp(2)%Re(1)" : 200.0
+        "run_time_info": "T",
+        "format": 1,
+        "precision": 2,
+        "prim_vars_wrt": "T",
+        "schlieren_wrt": "T",
+        "fd_order": 4,
+        "schlieren_alpha(1)": 20.0,
+        "schlieren_alpha(2)": 20.0,
+        "schlieren_alpha(3)": 20.0,
+        "E_wrt": "T",
+        "parallel_io": "T",
+        "x_domain%beg": -0.0015,
+        "x_domain%end": 0.0085,
+        "y_domain%beg": -0.003,
+        "y_domain%end": 0.003,
+        "z_domain%beg": -0.003,
+        "z_domain%end": 0.003,
+        "m": 200,
+        "n": 120,
+        "p": 120,
+        "cyl_coord": "F",
+        "cfl_adap_dt": "T",
+        "cfl_target": 0.5,
+        "n_start": 0,
+        "t_stop": 2.029784886350321e-05,
+        "t_save": 1e-07,
+        "num_patches": 2,
+        "num_fluids": 1,
+        "model_eqns": 2,
+        "alt_soundspeed": "F",
+        "mpp_lim": "F",
+        "mixture_err": "T",
+        "time_stepper": 3,
+        "weno_order": 5,
+        "weno_eps": 1e-16,
+        "weno_Re_flux": "T",
+        "weno_avg": "F",
+        "avg_state": 2,
+        "mapped_weno": "T",
+        "null_weights": "F",
+        "mp_weno": "F",
+        "riemann_solver": 2,
+        "wave_speeds": 1,
+        "viscous": "T",
+        "bc_x%beg": -3,
+        "bc_x%end": -3,
+        "bc_y%beg": -3,
+        "bc_y%end": -3,
+        "bc_z%beg": -3,
+        "bc_z%end": -3,
+        "patch_icpp(1)%geometry": 9,
+        "patch_icpp(1)%x_centroid": -0.00075,
+        "patch_icpp(1)%y_centroid": 0.0,
+        "patch_icpp(1)%z_centroid": 0.0,
+        "patch_icpp(1)%length_x": 0.0015,
+        "patch_icpp(1)%length_y": 0.006,
+        "patch_icpp(1)%length_z": 0.006,
+        "patch_icpp(1)%vel(1)": 114.51006445833742,
+        "patch_icpp(1)%vel(2)": 0.0,
+        "patch_icpp(1)%vel(3)": 0.0,
+        "patch_icpp(1)%pres": 159059.985,
+        "patch_icpp(1)%alpha_rho(1)": 1.657142223044202,
+        "patch_icpp(1)%alpha(1)": 1.0,
+        "patch_icpp(2)%geometry": 9,
+        "patch_icpp(2)%x_centroid": 0.00425,
+        "patch_icpp(2)%y_centroid": 0.0,
+        "patch_icpp(2)%z_centroid": 0.0,
+        "patch_icpp(2)%length_x": 0.0085,
+        "patch_icpp(2)%length_y": 0.006,
+        "patch_icpp(2)%length_z": 0.006,
+        "patch_icpp(2)%vel(1)": 0.0,
+        "patch_icpp(2)%vel(2)": 0.0,
+        "patch_icpp(2)%vel(3)": 0.0,
+        "patch_icpp(2)%pres": 101325.0,
+        "patch_icpp(2)%alpha_rho(1)": 1.204,
+        "patch_icpp(2)%alpha(1)": 1.0,
+        "ib": "T",
+        "num_ibs": 1,
+        "fluid_pp(1)%gamma": 2.5000000000000004,
+        "fluid_pp(1)%pi_inf": 0.0,
+        "fluid_pp(1)%Re(1)": 500.0,
+        "patch_ib(1)%geometry": 8,
+        "patch_ib(1)%x_centroid": 0.0009437121160009235,
+        "patch_ib(1)%y_centroid": -0.0005058431196531306,
+        "patch_ib(1)%z_centroid": 0.0010634125860177718,
+        "patch_ib(1)%radius": 0.00025,
+        "patch_ib(1)%slip": "F"
         }
     )
 )
